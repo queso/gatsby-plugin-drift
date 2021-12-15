7f26ef9b75b319e06bd5e75c7cd7b8eba35133a5
Josh Owens <joshua.owens@gmail.com>
2021-12-15 18:40:19 -0500

    Remove production var check from browser.

--- src/gatsby-browser.js
+++ src/gatsby-browser.js
@@ -1,5 +1,5 @@
 exports.onInitialClientRender = () => {
-  if (process.env.NODE_ENV === 'production' && typeof drift === 'object' && window.driftAppId) {
+  if (typeof drift === 'object' && window.driftAppId) {
     window.drift.SNIPPET_VERSION = '0.3.1';
     window.drift.load(window.driftAppId);
   }
